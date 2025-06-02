a7e0cdc7e356c73f95cc79b9ebd5c39160fef69a
limjong-wan <lim8848@gmail.com>
2025-06-03 01:55:11 +0900

    statefulwieget PageView pageController imageslide

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterstudy/screen/home_screen.dart';
-//import 'package:';
-/**
-    https://github.com/codefactory-co/flutter-golden-rabbit-novice-v3
- */
+
 void main() {
-  /**
-      앱 실행 전에 필요한 바인딩을 반드시 준비시키는 안전장치 역할
-      main() 함수에서 비동기 작업을 하기 전에 호출
-   */
-  WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
     MaterialApp(
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,61 +1,68 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 // URI/URL을 생성하는데 도움을 주는 클래스
 final uri = Uri.parse('https://blog.codefactory.ai');
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
-  /*
-      setNavigationDelegate
-       > 페이지가 로딩되거나 이동될 때 어떤 행동을 할지를 설정
-       > 웹페이지가 모두 로드된 후  print(url); 실행
-      loadRequest
-       > 실제로 웹 페이지를 로딩
-  */
-  WebViewController controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..setNavigationDelegate(NavigationDelegate(
-        onPageFinished: (String url){
-          print(url);
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController pageController = PageController();
+
+
+  @override
+  void initState(){
+    super.initState();
+
+    Timer.periodic(
+      Duration(seconds: 3),
+        (timer){
+          print('실행!');
+          int? nextPage = pageController.page?.toInt();
+          if (nextPage == null) {
+            return;
+          }
+          // ➌
+          if (nextPage == 3) {
+            nextPage = 0;
+          } else {
+            nextPage++;
+          }
+          pageController.animateToPage(
+            // ➍ 페이지 변경
+            nextPage,
+            duration: Duration(milliseconds: 500),
+            curve: Curves.ease,
+          );
+
         }
-    ))
-    ..loadRequest(uri); // ❶ 컨트롤러 변수 생성
+    );
+  }
 
-  /*
-      화면 UI 그리기 (build 함수)
-   */
   @override
   Widget build(BuildContext context) {
-    return Scaffold( //Scaffold - 앱 화면의 기본 구조를 잡는 위젯 (AppBar + Body)
-      appBar: AppBar(
-        // ➊ 앱바 위젯 추가
-        // ➋ 배경색 지정
-        backgroundColor: Colors.orange,
-        // ➌ 앱 타이틀 설정
-        title: Text('Code Factory'),
-        // ➍ 가운데 정렬
-        centerTitle: true,
-        actions: [
-          IconButton(
-            // ➋ 눌렀을 때 콜백 함수 설정
-            onPressed: () {
-              // ➌ 웹뷰에서 보여줄 사이트 실행하기
-              controller.loadRequest(Uri.parse('https://m.naver.com'));
-            },
 
-            // ➍ 홈 버튼 아이콘 설정
-            icon: Icon(
-              Icons.home,
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
+    return Scaffold(
+      body: PageView(
+          controller: pageController,
+          children: [1,2,3,4]
+              .map(
+                (number) => Image.asset(
+              'assets/image_$number.png',
+              fit: BoxFit.cover,
             ),
-          ),
-        ],
-      ),
-      body: WebViewWidget(
-        // ❷ WebView 추가하기
-        controller: controller, //Flutter 앱의 화면(body)에 웹사이트를 보여주는 코드
+          ).toList()
       ),
     );
   }
+
 }
